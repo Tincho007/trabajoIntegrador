120d7413b504b5fe334bc22bcdd3602993242a53
Nicolas Rivera <nicolasrivera.dg@gmail.com>
2016-10-22 12:02:27 -0300

    Archivos PHP bien subidos en carpeta

---
+++ tp/header.php
@@ -0,0 +1,17 @@
+<?php ?>
+  <header>
+    <h1 id="name2">Solucionado</h1>
+    <div id="logoSolucionado"><a id="link_logo" href="index.php" target="_self"><img src="img/logo_solucionado.png" class="logo_header"></a></div>
+    <nav class="main-nav">
+      <ul>
+        <li><a href="registration.php" target="_self">Registro</a></li>
+        <li><a href="faq.php" target="_self">FAQ</a></li>
+      </ul>
+    </nav>
+    <form id="loginHome" action="" method="post">
+      <input id="mailLog" name="mailLog" placeholder=" mail">
+      <input id="passLog" type="password" name="passLog" placeholder=" contraseña">
+      <button id="ingresar" type="submit" name="ingresar">Ingresar</button>
+    </form>
+  </header>
+<?php ?>
